7418a970a8fc5aea5a3779e0fd13ac731348d132
Denise Recofka <recofka@gmail.com>
2024-07-14 17:55:03 +0200

    Adding graph component

--- src/components/Graph/__tests__/Graph.test.tsx
+++ src/components/Graph/__tests__/Graph.test.tsx
@@ -1,34 +1,28 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
-// import '@testing-library/jest-dom/extend-expect';
 import Graph from '../Graph';
-import { DataPoint } from '@/types/types';
 
-// Mock data for testing
-const mockData: DataPoint[] = [
-  { timestamp: '2023-07-01', value: 10 },
-  { timestamp: '2023-07-02', value: 20 },
-  { timestamp: '2023-07-03', value: 30 }
+const mockData = [
+  { timestamp: '2024-07-01', value: 1126.3970050016592 },
+  { timestamp: '2024-07-02', value: 1233.4099879467929 },
+  { timestamp: '2024-07-03', value: 1049.2452635057316 }
 ];
 
-test('renders the Graph component with title', () => {
-  render(<Graph title="Test Graph" data={mockData} />);
-  const titleElement = screen.getByText(/Test Graph/i);
-  expect(titleElement).toBeInTheDocument();
+describe('Graph Component', () => {
+  it('should renders the Graph component with title', () => {
+    render(<Graph title="Test Graph" data={mockData} />);
+    const titleElement = screen.getByText(/Test Graph/i);
+    expect(titleElement).toBeInTheDocument();
+  });
+
+  it('should render the graph with correct data', () => {
+    render(
+      <Graph data={mockData} title="Test Graph" lineColor="#8884d8" />
+    );
+
+    const element = screen.getByText(/2024-07-01/i);
+    expect(element).toBeInTheDocument();
+  });
+
 });
 
-test('renders the correct number of bars in the graph', () => {
-  render(<Graph title="Test Graph" data={mockData} />);
 
-  // Check if BarChart receives the correct props
-  const bars = screen.getAllByRole('bar');
-  expect(bars).toHaveLength(mockData.length);
-});
-
-test('renders the Graph component with custom line color', () => {
-  render(<Graph title="Colored Graph" data={mockData} lineColor="red" />);
-  const graphElement = screen.getByTestId('graph-element');
-
-  // Check if the lineColor is applied
-  expect(graphElement).toHaveStyle('color: red');
-});
